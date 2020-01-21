cecd6fb9e7251668eb429e6c61be7ba85c7eb714
edward <es916@ic.ac.uk>
2020-01-21 11:41:08 +0000

    Add Log output for unknown test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,6 +3,7 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
@@ -27,6 +28,8 @@ public class QueryProcessor {
             }
             return Integer.toString(m);
         }
+
+        System.out.println("Warning: Unknown query: \"" + query + "\"");
         return "";
     }
 }
