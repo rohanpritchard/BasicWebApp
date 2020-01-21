35350b06afe6c9b6829c8a283d785173da558a20
Georgi Zhelev <gz1016@ic.ac.uk>
2020-01-21 11:40:10 +0000

    fix syntax

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -34,6 +34,7 @@ public class QueryProcessorTest {
     @Test
     public void knowsAboutPlus() throws Exception {
         assertThat(queryProcessor.process("what is 1 plus 2"), containsString("3"));
+    }
     @Test
     public void knowsAboutTeamName() throws Exception {
         assertThat(queryProcessor.process("what is your team name"), containsString("The Egg Deluxe"));
